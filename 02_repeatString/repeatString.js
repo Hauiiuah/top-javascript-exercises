d023400d6af9740142275ace7218fad8e4f07e2b
Jan Haubrock <jan.haubrock@live.com>
2022-06-21 23:24:59 +0200

    helloWorld and repeatString complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str,count) {
+    let rString=''
+    if(count < 0) return 'ERROR'
+    for(let i=0;i<count;i++)
+    {
+        rString+=str
+    }
 
+    return rString
 };
 
 // Do not edit below this line
