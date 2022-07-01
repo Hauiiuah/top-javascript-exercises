37b6ac6745f776e0b16b42f603022381d2c96e6c
Jan Haubrock <jan.haubrock@live.com>
2022-07-02 01:26:07 +0200

    finish exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+Array.prototype.equals = function (array) {
+  if (!array) return false
 
-};
+  if (this.length != array.length) return false
+
+  let result = true
+  this.forEach((l, index) => {
+    if (l !== array[index]) {
+      result = false
+      return
+    }
+  })
+
+  return result
+}
+
+const palindromes = (str) => {
+  const letters = str.toLowerCase().match(/[A-Z]/gi)
+  const reverse = Array.from(letters)
+  reverse.reverse()
+
+  return letters.equals(reverse)
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
