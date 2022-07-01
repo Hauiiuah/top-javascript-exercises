1829d63b3839a307685b61731a8a7cf851e16d6f
Jan Haubrock <jan.haubrock@live.com>
2022-07-02 00:55:12 +0200

    finish exercise 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,26 @@
-const add = function() {
-	
-};
+const add = (num1, num2) => {
+  return num1 + num2
+}
 
-const subtract = function() {
-	
-};
+const subtract = (num1, num2) => {
+  return num1 - num2
+}
 
-const sum = function() {
-	
-};
+const sum = (arr) => {
+  return arr.reduce((prev, curr) => prev + curr, 0)
+}
 
-const multiply = function() {
+const multiply = (arr) => {
+  return arr.reduce((prev, curr) => prev * curr, 1)
+}
 
-};
+const power = (num, exp) => {
+  return num ** exp
+}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = (f) => {
+  return f === 0 ? 1 : f * factorial(f - 1)
+}
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +29,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
-};
+  factorial,
+}
