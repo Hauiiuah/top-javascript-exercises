76add0597fd99d094a49fe7bdcbbeab4e10f5067
Jan Haubrock <jan.haubrock@live.com>
2022-07-02 01:42:41 +0200

    finish exercise 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
-};
+const fibonacci = (num) => {
+  if (num < 0) return 'OOPS'
+  if (num == 1 || num == 2) return 1
+  return fibonacci(num - 1) + fibonacci(num - 2)
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
+
+/*
+
+fib(4) =>
+  return fib(3)+fib(2)
+  fib(3) =>
+    return fib(2)+fib(1)
+    fib(2) =>
+      return fib(1)+fib(0)
+      fib(1) => 1
+      fib(0) => 1
+    => 2
+    fib(1) =>1
+
+*/
