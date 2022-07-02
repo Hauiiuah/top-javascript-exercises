aff454680310bdabba7bce560453ec1843ea9235
Jan Haubrock <jan.haubrock@live.com>
2022-07-02 02:03:47 +0200

    finish exercise 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
+const calcAge = (person) => {
+  const birth = person.yearOfBirth
+  const death = person.yearOfDeath || new Date().getFullYear()
 
-};
+  return death - birth
+}
+
+const findTheOldest = (people) =>
+  people.reduce((prev, cur) => (calcAge(prev) > calcAge(cur) ? prev : cur))
 
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
